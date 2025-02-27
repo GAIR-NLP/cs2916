fa22d49586813f71c2c26f24e1011a7f3619a237
Pengfei Liu <59123869+pfliu-nlp@users.noreply.github.com>
2025-02-27 12:41:04 +0800

    update baseurl

---
+++ assets/js/053b6def.e00c5441.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[5950],{4377:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/cs2916/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.7575746d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/cs2916/blog","blogTitle":"Blog","authorsListPath":"/cs2916/blog/authors"}')}}]);
